bb2040fdf515295fb2822e1b1df0cba336a200f7
Sheri Ellis <sheri.c.ellis@gmail.com>
2017-07-04 22:54:54 -0700

    Add project files.

---
+++ CourtFeedback4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourtFeedback4.MvcApplication" Language="C#" %>
---
+++ CourtFeedback4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourtFeedback4.Startup))]
+namespace CourtFeedback4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
